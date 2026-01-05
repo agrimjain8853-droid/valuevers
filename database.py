23a68ff64e237982478cf10757ce8f90ab012234
agrimjain8853-droid <agrimjain8853@gmail.com>
2026-01-05 15:19:16 +0530

    database.py

--- database.py
+++ database.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import sessionmaker
 from models import Base
 
 # Read from environment variable (REQUIRED for deployment)
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = os.getenv("postgresql://postgres:q-LN5VfKQmP?ka2@db.isfdhktoalmzbbtjfvil.supabase.co:5432/postgres")
 
 if not DATABASE_URL:
     raise RuntimeError("DATABASE_URL is not set")
